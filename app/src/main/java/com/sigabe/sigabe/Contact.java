baef421808870f3c4658d1f01ffa3752a4f1c4be
deykusuma <kusumadeyana@gmail.com>
2019-01-05 16:56:15 +0700

    New Design Layout Contact and Form

--- app/src/main/java/com/sigabe/sigabe/Contact.java
+++ app/src/main/java/com/sigabe/sigabe/Contact.java
@@ -1,18 +1,38 @@
 package com.sigabe.sigabe;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.sigabe.sigabe.Content.SurvivalDashboardTab;
+import com.sigabe.sigabe.Emergency.MapsActivity;
 import com.sigabe.sigabe.R;
 
 public class Contact extends Fragment {
+    FloatingActionButton addFriend;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_contact,null);
+    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addFriend = (FloatingActionButton) view.findViewById(R.id.addFriend);
+
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), FormAddFriend.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/sigabe/sigabe/FormAddFriend.java
@@ -0,0 +1,13 @@
+package com.sigabe.sigabe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormAddFriend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_add_friend);
+    }
+}
